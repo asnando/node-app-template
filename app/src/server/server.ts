5154dfdca0a497eef266dce1294147f4cec58ae7
Fernando Rama <fernandorama.dev@gmail.com>
2020-05-15 02:26:36 -0300

    Wrap web server with shutdown method

--- app/src/server/server.ts
+++ app/src/server/server.ts
@@ -4,6 +4,8 @@ import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import promBundle from 'express-prom-bundle';
+import http, { Server } from 'http';
+import createShutdownWrapper from 'http-shutdown';
 import router from './router';
 import getEnv from '../utils/env';
 import { print, printRequestStream } from '../logger';
@@ -14,23 +16,33 @@ const metricsMiddleware = promBundle({
   includePath: true,
 });
 
-function createServer(serverPort: number = getEnv('SERVER_PORT', 8080)): Promise<ExpressApplication> {
+export interface IExpressApplicationWithShutdown extends Server {
+  shutdown: Function,
+};
+
+function createServer(serverPort: number = getEnv('SERVER_PORT', 8080)): Promise<IExpressApplicationWithShutdown> {
   return new Promise((resolve, reject) => {
     const onServerUp = (() => {
       print(`Server up and running at ${serverPort} port.`);
-      resolve(server);
     });
 
-    const server: ExpressApplication = express();
+    const app: ExpressApplication = express();
 
-    server
+    let server = http.createServer(app) as IExpressApplicationWithShutdown;
+    server = createShutdownWrapper(server);
+
+    app
       .use(helmet())
       .use(cors())
       .use(morgan('tiny', { stream: printRequestStream }))
       .use(bodyParser.json())
       .use(metricsMiddleware)
-      .use(router)
+      .use(router);
+
+    server
       .listen(serverPort, onServerUp);
+
+    resolve(server);
   });
 }
 
