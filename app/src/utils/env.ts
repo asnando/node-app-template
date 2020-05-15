6b311f86e00e298d7e3ee776e689d725c2b4a26d
Fernando Rama <fernandorama.dev@gmail.com>
2020-05-15 03:59:32 -0300

    Improve code coverage

--- app/src/utils/env.ts
+++ app/src/utils/env.ts
@@ -2,8 +2,5 @@ require('dotenv').config();
 
 export default function getEnv(name: string, dflt: any) {
   const { env } = process;
-  if (env[name]) {
-    return env[name];
-  }
-  return dflt || null;
+  return env[name] || dflt;
 }
